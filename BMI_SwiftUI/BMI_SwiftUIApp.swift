df95835d1f8ef2e36619781cf64950de608434de
Jisoo Park <scalett20041@gmail.com>
2022-08-16 09:26:47 +0900

    Initial Commit

---
+++ BMI_SwiftUI/BMI_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMI_SwiftUIApp.swift
+//  BMI_SwiftUI
+//
+//  Created by 박지수 on 2022/08/16.
+//
+
+import SwiftUI
+
+@main
+struct BMI_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
